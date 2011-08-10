30d1fda9ed445d64dc1208c7ff3ec9b40ce0e722
chuzheng-windows <chuzhengcn@gmail.com>
2011-08-10 17:35:50 +0800

    modify failure style

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -3,5 +3,5 @@ class Attachment < ActiveRecord::Base
   belongs_to :task_history
   has_attached_file :pic
   has_attached_file :document
-  #validates_attachment_content_type :pic, :content_type => 'image/png'  
+  #validates_attachment_content_type :document, :content_type => 'image/png'  
 end
